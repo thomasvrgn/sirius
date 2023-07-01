20359c366b2218a115dddeeef9fe86ce8583f5a9
thomasvergne <thomas.vergne.contact@gmail.com>
2023-07-01 15:09:09 +0200

    feat: added module resolver.

---
+++ src/Language/Sirius/Module/Resolver.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+module Language.Sirius.Module.Resolver where
+import Language.Sirius.CST.Modules.Located
+import Language.Sirius.CST.Expression
+import Control.Monad.Except
+import Language.Sirius.Parser
+import System.Directory
+import System.FilePath
+
+type MonadResolver m = (MonadIO m, MonadError (Text, Position) m)
+
+getSiriusPath :: MonadResolver m => Position -> m String
+getSiriusPath pos = do
+  path <- liftIO $ lookupEnv "SIRIUS"
+  case path of
+    Just path' -> return path'
+    Nothing -> throwError ("Could not find SIRIUS environment variable", pos)
+
+resolveImport :: MonadResolver m => Text -> Text -> Position -> m [Located Toplevel]
+resolveImport dir name pos = do
+  stdPath <- getSiriusPath pos
+  let stdName = stdPath </> toString name
+  let path = if "std" `isPrefixOf` toString name then stdName else toString dir </> toString name
+
+  fileExists <- liftIO $ doesFileExist path
+  unless fileExists $ throwError ("Could not find module " <> name, pos)
+
+  content <- decodeUtf8 <$> liftIO (readFileBS path)
+  ast <- parseSirius (toString name) content
+  case ast of
+    Left err -> liftIO $ print err >> exitFailure
+    Right ast' -> resolveImports (fromString $ takeDirectory path) ast'
+
+-- | Resolve the imports of a module.
+resolveImports :: MonadResolver m => Text -> [Located Toplevel] -> m [Located Toplevel]
+resolveImports dir (Located pos (TUse name) : toplevels) = do
+  resolved <- resolveImport dir (fromString $ intercalate "/" (map toString name)) pos
+  rest <- resolveImports dir toplevels
+  return $ (TNamespace "_" resolved :>: pos) : rest
+resolveImports dir (toplevel : toplevels) = do
+  rest <- resolveImports dir toplevels
+  return $ toplevel : rest
+resolveImports _ [] = return []
+
+runModuleResolver :: MonadIO m => Text -> [Located Toplevel] -> m (Either (Text, Position) [Located Toplevel])
+runModuleResolver dir toplevels = runExceptT (resolveImports (fromString $ takeDirectory (toString dir)) toplevels)
