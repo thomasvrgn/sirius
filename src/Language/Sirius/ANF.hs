06909add87e60455e9868f67b716d548c406717a
thomasvergne <thomas.vergne.contact@gmail.com>
2023-07-12 19:26:25 +0200

    feat: updated ANF execution function.

--- src/Language/Sirius/ANF.hs
+++ src/Language/Sirius/ANF.hs
@@ -6,4 +6,4 @@ import qualified Language.Sirius.Typecheck.Definition.AST as C
 import qualified Control.Monad.RWS as ST
 
 runANFPass :: Monad m => [C.Toplevel] -> m [ANF.Toplevel]
-runANFPass xs = fst <$> ST.evalRWST (mapM convertToplevel xs) [] 0
+runANFPass xs = fst <$> ST.evalRWST (mapM (convertToplevel) xs) [] (0, [])
